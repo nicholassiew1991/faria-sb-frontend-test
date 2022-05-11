c0df6d663b19ad65877f05567f532bd5f1a3c423
nicholassiew1991 <nicholas_siew1991@hotmail.com>
2022-05-11 18:20:22 +0800

    load asset data

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,51 @@
 import { Injectable } from '@angular/core';
+import { User } from 'src/app/models/user';
+import UsersJson from '../../assets/users.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private users: User[] = []
+
+  constructor() {
+    this.users = (UsersJson as []).map(x => this.rawDataUserMapper(x));
+  }
+
+  private rawDataUserMapper(obj: any) : User {
+    return {
+      name: obj.name,
+      age: obj.age,
+      email: obj.email,
+      balance: Number(obj.balance.replace(',', '')),
+      registered: this.parseRegisteredDateTime(obj.registered),
+      _id: obj._id,
+      index: obj.index,
+      guid: obj.guid,
+      isActive: obj.isActive,
+      picture: obj.picture,
+      eyeColor: obj.eyeColor,
+      gender: obj.gender,
+      company: obj.company,
+      phone: obj.phone,
+      address: obj.address,
+      about: obj.about,
+      latitude: obj.latitude,
+      longitude: obj.longitude,
+      tags: obj.tags,
+      friends: obj.friends,
+      greeting: obj.greeting,
+      favoriteFruit: obj.favoriteFruit
+    } as User
+  }
+
+  private parseRegisteredDateTime(str: string) : Date {
+
+    let lastColonIndex = str.lastIndexOf(':');
+    str = str.slice(0, lastColonIndex) + str.slice(lastColonIndex + 1);
+    str = str.replace(' ', '');
+
+    return new Date(Date.parse(str))
+  }
 }
